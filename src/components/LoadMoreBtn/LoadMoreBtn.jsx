8b9df0cce1d3968c74e7b564f9224f370245da78
Vitalii Artiukh <radial_car@yahoo.com>
2024-10-10 23:35:58 +0300

    start

---
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const LoadMoreBtn = () => {
+  return <div>LoadMoreBtn</div>;
+};
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const SearchBar = () => {
+  return (
+    <header>
+      <form>
+        <input
+          type="text"
+          autocomplete="off"
+          autofocus
+          placeholder="Search images and photos"
+        />
+        <button type="submit">Search</button>
+      </form>
+    </header>
+  );
+};
